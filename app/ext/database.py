b238e0299e7e51034624ad890594a911c43685fd
gustavoneves1 <40774449+gustavoneves1@users.noreply.github.com>
2024-09-11 19:06:06 -0300

    Feature: adjust

--- app/ext/database.py
+++ app/ext/database.py
@@ -1,11 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import psycopg2
+from urllib.parse import quote_plus
+
+
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def init_database(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345678@localhost:5432/desafios_database?client_encoding=utf8'
+    password = quote_plus("@Gnp@040794_")
+#    Construir a URI de conexão com a senha codificada
+    connection_uri = f"postgresql://postgres.tnhjnemsurdckpipskks:{password}@aws-0-us-west-1.pooler.supabase.com:6543/postgres?client_encoding=utf8"
+    app.config['SQLALCHEMY_DATABASE_URI'] = connection_uri
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
